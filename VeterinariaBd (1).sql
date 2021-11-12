b67e55562540d1c4c81371742de2a6f10ebdffe3
FranSolorzano <87993086+FranSolorzano@users.noreply.github.com>
2021-11-12 10:15:25 -0600

    d

--- VeterinariaBd (1).sql
+++ VeterinariaBd (1).sql
@@ -54,16 +54,10 @@ Observacion nvarchar(255) not null
 create table Ingresos(
 Id int primary key identity (1,1) not null,
 Motivo varchar(255) not null,
-FechaIngreso int foreign key references FechaIngreso(IdFechaIngreso) not null,
+FechaIngreso int not null,
 Observaciones varchar (255) not null
 );
 
-create table FechaIngreso(
-IdFechaIngreso int primary key identity (1,1) not null,
-Dia int not null,
-Mes int not null,
-Año int not null
-);
 
 /*Veterinario*/
 create table Veterinario(
@@ -80,14 +74,32 @@ insert into Veterinario(Nombre,Apellidos, Cargo) values (' Rene',' Valdivieso', 
 insert into Veterinario(Nombre,Apellidos, Cargo) values (' Franklin',' Solorzano', 'Veterinario');
 
 /*INSER INTO 10 MASCOTA*/
-insert into Mascota(Nombre,tipoMascota, peso, raza) values (' kobe',' Figueroa', 'Veterinario');
-insert into Mascota(Nombre,tipoMascota, peso, raza) values (' Luchy',' Sandoval', 'Veterinario');
-insert into Mascota(Nombre,tipoMascota, peso, raza) values (' nini',' Valdivieso', 'Veterinario');
-insert into Mascota(Nombre,tipoMascota, peso, raza) values (' Firulay',' Solorzano', 'Veterinario');
-insert into Mascota(Nombre,tipoMascota, peso, raza) values (' Copito',' Valdivieso', 'Veterinario');
-insert into Mascota(Nombre,tipoMascota, peso, raza) values (' tinti',' Solorzano', 'Veterinario');
-insert into Mascota(Nombre,tipoMascota, peso, raza) values (' Fredy',' Figueroa', 'Veterinario');
-insert into Mascota(Nombre,tipoMascota, peso, raza) values (' Luciana',' Sandoval', 'Veterinario');
-insert into Mascota(Nombre,tipoMascota, peso, raza) values (' Rene',' Valdivieso', 'Veterinario');
-insert into Mascota(Nombre,tipoMascota, peso, raza) values (' Franklin',' Solorzano', 'Veterinario');
+insert into Mascota(Nombre,tipoMascota, peso, raza) values (' kobe',' perro',15, 'husky');
+insert into Mascota(Nombre,tipoMascota, peso, raza) values (' Luchy',' gato',6, 'cola de ardilla');
+insert into Mascota(Nombre,tipoMascota, peso, raza) values (' nini',' perico',2, 'australiano');
+insert into Mascota(Nombre,tipoMascota, peso, raza) values (' Firulay',' perro',20, 'doberman');
+insert into Mascota(Nombre,tipoMascota, peso, raza) values (' Copito',' conejo',5, 'de campo');
+insert into Mascota(Nombre,tipoMascota, peso, raza) values (' tinti',' gato',10, 'Cola de ardilla');
+insert into Mascota(Nombre,tipoMascota, peso, raza) values (' nini',' perro',15, 'rotwailer');
+insert into Mascota(Nombre,tipoMascota, peso, raza) values (' Firulay',' perro',15, 'Labrador');
+insert into Mascota(Nombre,tipoMascota, peso, raza) values (' Copito',' gato',10, 'cola de ardilla');
+insert into Mascota(Nombre,tipoMascota, peso, raza) values (' tinti',' gato',10, 'cola de ardilla');
+
+
+/*INSER INTO 14 INGRESOS*/
+insert into Ingresos(Motivo,FechaIngreso,Observaciones) Values ('Rabia','25/04/21','Rabioso')
+insert into Ingresos(Motivo,FechaIngreso,Observaciones) Values ('Paracitos','22/04/21','Sin desaparacitar')
+insert into Ingresos(Motivo,FechaIngreso,Observaciones) Values ('Vacuna','29/03/21','Sin la segunda vacuna')
+insert into Ingresos(Motivo,FechaIngreso,Observaciones) Values ('Dolor de pancita','22/06/21','Se comio una mala comida')
+insert into Ingresos(Motivo,FechaIngreso,Observaciones) Values ('Paracitos','12/02/21','Sin desaparacitar')
+insert into Ingresos(Motivo,FechaIngreso,Observaciones) Values ('Dolor de pancita','22/03/21','Se comio un animal venenoso')
+insert into Ingresos(Motivo,FechaIngreso,Observaciones) Values ('Vacuna','25/03/21','Sin la primera vacuna')
+insert into Ingresos(Motivo,FechaIngreso,Observaciones) Values ('Rabia','21/08/21','Rabioso desde hace una semana')
+insert into Ingresos(Motivo,FechaIngreso,Observaciones) Values ('Rabia','22/07/21','Rabioso desde hace dos meses')
+insert into Ingresos(Motivo,FechaIngreso,Observaciones) Values ('Rabia','12/05/21','Rabioso desde hace una semana')
+insert into Ingresos(Motivo,FechaIngreso,Observaciones) Values ('Dolor de patita derecha','22/03/21','Le paso un carro')
+insert into Ingresos(Motivo,FechaIngreso,Observaciones) Values ('Vacuna','10/03/21','Rabioso')
+insert into Ingresos(Motivo,FechaIngreso,Observaciones) Values ('Paracitos','05/03/21','Sin desparacitar')
+insert into Ingresos(Motivo,FechaIngreso,Observaciones) Values ('Rabia','15/03/21','Rabioso')
+
 
